bc44c53541f6a5c08090aa90ca9ace8df582e1ab
Muhammad Farhan Ramadhan <75202928+farhan1499@users.noreply.github.com>
2021-08-26 06:11:52 +0700

    Add files via upload

---
+++ function.php
@@ -0,0 +1,101 @@
+<?php
+session_start();
+
+//Membuat koneksi ke database
+$conn = mysqli_connect("localhost","root","","stockbarang");
+
+//Menambah barang baru
+if( isset( $_POST['addnewbarang'] ) ) {
+    $namabarang = $_POST['namabarang'];
+    $deskripsi = $_POST['deskripsi'];
+    $stock = $_POST['stock'];
+
+    $addtotable = mysqli_query( $conn, "INSERT INTO stock(namabarang, deskripsi, stock) VALUES('$namabarang', '$deskripsi', '$stock')" );
+    
+    if( $addtotable ) {
+        header( 'location:index.php' );
+    }else {
+        echo 'Gagal';
+        header( 'location:index.php' );
+    }
+}
+
+//Menambah barang masuk
+if( isset($_POST['barangmasuk'] ) ) {
+    $databarangmasuk = $_POST['databarangmasuk'];
+    $penerima = $_POST['penerima'];
+    $qty = $_POST['qty'];
+
+    $cekstocksekarang = mysqli_query( $conn, "SELECT * FROM stock WHERE idbarang='$databarangmasuk'");
+    $ambildata = mysqli_fetch_array( $cekstocksekarang );
+
+    $stocksekarang = $ambildata['stock'];
+    $tambahstock = $stocksekarang+$qty;
+
+
+    $addtomasuk = mysqli_query( $conn, "INSERT INTO masuk(idbarang, keterangan, qty) VALUES('$databarangmasuk', '$penerima', '$qty')" );
+    $updatestockmasuk = mysqli_query( $conn, "UPDATE stock SET stock='$tambahstock' WHERE idbarang='$databarangmasuk'");
+    
+    if( $addtomasuk && $updatestockmasuk ) {
+        header( 'location:masuk.php' );
+    }else {
+        echo 'Gagal';
+        header( 'location:masuk.php' );
+    }
+}
+
+//Menambah barang keluar
+if( isset($_POST['addbarangkeluar'] ) ) {
+    $databarangkeluar = $_POST['databarangkeluar'];
+    $penerima = $_POST['penerima'];
+    $qty = $_POST['qty'];
+
+    $cekstocksekarang = mysqli_query( $conn, "SELECT * FROM stock WHERE idbarang='$databarangkeluar'");
+    $ambildata = mysqli_fetch_array( $cekstocksekarang );
+
+    $stocksekarang = $ambildata['stock'];
+    $kurangstock = $stocksekarang-$qty;
+
+
+    $addtokeluar = mysqli_query( $conn, "INSERT INTO keluar(idbarang, penerima, qty) VALUES('$databarangkeluar', '$penerima', '$qty')" );
+    $updatestockkeluar = mysqli_query( $conn, "UPDATE stock SET stock='$kurangstock' WHERE idbarang='$databarangkeluar'");
+    
+    if( $addtokeluar && $updatestockkeluar ) {
+        header( 'location:keluar.php' );
+    }else {
+        echo 'Gagal';
+        header( 'location:keluar.php' );
+    }
+}
+
+//Update info barang
+if( isset( $_POST['updatebarang'] ) ) {
+    $idb = $_POST['idb'];
+    $namabarang = $_POST['namabarang'];
+    $desk = $_POST['deskripsi'];
+
+    $update = mysqli_query( $conn, "UPDATE stock SET namabarang='$namabarang', deskripsi='$desk' WHERE idbarang='$idb'");
+    if( $update ) {
+        header("location:index.php");
+    }else {
+        echo "Gagal Update";
+        header("location:index.php");
+    }
+}
+
+//Menghapus barang
+if( isset($_POST['hapusbarang'] ) ) {
+    $idb = $_POST['idb'];
+
+    $hapus = mysqli_query( $conn, "DELETE FROM stock WHERE idbarang='$idb'");
+
+    if( $hapus ) {
+        header('location:index.php');
+    }else {
+        echo 'Gagal Hapus';
+        header('location:index.php');
+    }
+}
+
+
+?>
---
+++ stockbarang.sql
@@ -0,0 +1,168 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 26, 2021 at 01:11 AM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `stockbarang`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `keluar`
+--
+
+CREATE TABLE `keluar` (
+  `idkeluar` int(11) NOT NULL,
+  `idbarang` int(11) NOT NULL,
+  `tanggal` timestamp NOT NULL DEFAULT current_timestamp(),
+  `penerima` varchar(25) NOT NULL,
+  `qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `keluar`
+--
+
+INSERT INTO `keluar` (`idkeluar`, `idbarang`, `tanggal`, `penerima`, `qty`) VALUES
+(1, 1, '2021-08-24 06:42:55', 'Udin', 150),
+(2, 3, '2021-08-24 10:17:20', 'Farhan', 55);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `iduser` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`iduser`, `email`, `password`) VALUES
+(1, 'myfarhan.ramadhan@gmail.com', '11111111');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `masuk`
+--
+
+CREATE TABLE `masuk` (
+  `idmasuk` int(11) NOT NULL,
+  `idbarang` int(11) NOT NULL,
+  `tanggal` timestamp NOT NULL DEFAULT current_timestamp(),
+  `keterangan` varchar(25) NOT NULL,
+  `qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `masuk`
+--
+
+INSERT INTO `masuk` (`idmasuk`, `idbarang`, `tanggal`, `keterangan`, `qty`) VALUES
+(5, 1, '2021-08-24 04:32:23', 'Farhan', 100),
+(6, 1, '2021-08-24 04:33:28', 'Eko', 200),
+(7, 3, '2021-08-24 10:17:01', 'Udin', 50);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stock`
+--
+
+CREATE TABLE `stock` (
+  `idbarang` int(11) NOT NULL,
+  `namabarang` varchar(25) NOT NULL,
+  `deskripsi` varchar(25) NOT NULL,
+  `stock` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `stock`
+--
+
+INSERT INTO `stock` (`idbarang`, `namabarang`, `deskripsi`, `stock`) VALUES
+(1, 'Realme 8', 'Smartphone', 150),
+(2, 'Asus E1', 'Laptop', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `keluar`
+--
+ALTER TABLE `keluar`
+  ADD PRIMARY KEY (`idkeluar`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`iduser`);
+
+--
+-- Indexes for table `masuk`
+--
+ALTER TABLE `masuk`
+  ADD PRIMARY KEY (`idmasuk`);
+
+--
+-- Indexes for table `stock`
+--
+ALTER TABLE `stock`
+  ADD PRIMARY KEY (`idbarang`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `keluar`
+--
+ALTER TABLE `keluar`
+  MODIFY `idkeluar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `iduser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `masuk`
+--
+ALTER TABLE `masuk`
+  MODIFY `idmasuk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `stock`
+--
+ALTER TABLE `stock`
+  MODIFY `idbarang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
