f40f127225e10c46f71e6d40807900bab7b21c8d
farhan1499 <myfarhan.ramadhan@gmail.com>
2021-08-27 15:25:30 +0700

    Penambahan file regist.php

--- regist.php
+++ regist.php
@@ -1,36 +1,3 @@
-<?php
-
-require_once "function.php";
-
-//Cek Login
-if (isset($_POST['login'])) {
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-
-    //Cek Data di database
-    $cekdatabase = mysqli_query($conn, "SELECT * FROM login where email='$email' and password='$password'");
-    //Hitung data
-    $hitung = mysqli_num_rows($cekdatabase);
-
-    if ($hitung > 0) {
-        $data = mysqli_fetch_assoc($cekdatabase);
-
-        $_SESSION['log'] = 'True';
-        $_SESSION['level'] = $data['level'];
-        $_SESSION['iduser'] = $data['iduser'];
-        header("location:index.php");
-    } else {
-        header("location:login.php");
-    };
-};
-
-if (!isset($_SESSION['log'])) {
-} else {
-    header("location:index.php");
-};
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -40,7 +7,7 @@ if (!isset($_SESSION['log'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Login - SB Admin</title>
+    <title>Register - RCS Website</title>
     <link href="css/styles.css" rel="stylesheet" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
 </head>
@@ -54,25 +21,29 @@ if (!isset($_SESSION['log'])) {
                         <div class="col-lg-5">
                             <div class="card shadow-lg border-0 rounded-lg mt-5">
                                 <div class="card-header">
-                                    <h3 class="text-center font-weight-light my-4">Login</h3>
+                                    <h3 class="text-center font-weight-light my-4">Register</h3>
                                 </div>
                                 <div class="card-body">
                                     <form method="post">
                                         <div class="form-floating mb-3">
                                             <input class="form-control" name="email" id="inputEmail" type="email" placeholder="name@example.com" />
-                                            <label for="inputEmail">Email address</label>
+                                            <label for="inputEmail">Your Email</label>
                                         </div>
                                         <div class="form-floating mb-3">
                                             <input class="form-control" name="password" id="inputPassword" type="password" placeholder="Password" />
                                             <label for="inputPassword">Password</label>
                                         </div>
+                                        <div class="form-floating mb-3">
+                                            <input class="form-control" name="level" id="inputLevel" type="number" placeholder="Level" />
+                                            <label for="inputLevel">Choose Your Level 1/2</label>
+                                        </div>
                                         <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
-                                            <button class="btn btn-primary" name="login">Login</button>
+                                            <button class="btn btn-primary" name="regist">Regist</button>
                                         </div>
                                     </form>
                                 </div>
                                 <div class="card-footer text-center py-3">
-                                    <div class="small"><a href="regist.php">Need an account? Sign up!</a></div>
+                                    <div class="small"><a href="login.php">Back to Login</a></div>
                                 </div>
                             </div>
                         </div>
